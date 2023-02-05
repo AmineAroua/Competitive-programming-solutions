8993941566552b86e9238fe4393277ee7381e6c1
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:46:28 +0100

    Committing files

---
+++ CodeForces/GNU C++17/496C | Removing Columns/179114868.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) (v.begin(),v.end())
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int n,m;
+    cin>>n>>m;
+    vector<string> v(n);
+    set<int> col;
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int lst = -1;
+    while(lst!=col.size())
+    {
+        lst = col.size();
+        for(int i=0;i<n-1;i++)
+        {
+ 
+            string s = v[i];
+            string t = v[i+1];
+            for(int j=0;j<m;j++)
+            {
+                if(col.count(j))continue;
+                if(s[j]<t[j])
+                {
+                    break;
+                }
+                if(s[j]>t[j])
+                {
+                    col.insert(j);
+                }
+            }
+        }
+    }
+    cout<<col.size()<<nl;
+}
+ 
