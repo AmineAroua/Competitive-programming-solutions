de4dfd603a41752cdc9375cc25bd0fa0bf9f0b01
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:57:32 +0100

    Committing files

---
+++ CodeForces/GNU C++17/227C | Flying Saucer Segments/173216346.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+int modn(int x,int n,int y)
+{
+    if(n==0)
+    {
+        return 1%y;
+    }
+    ll u = modn(x,n/2,y);
+    u = (u*u)%y;
+    if(n%2==1)
+    {
+        u = (u*x)%y;
+    }
+    return u;
+}
+ 
+//functions:
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int n,m;
+    cin>>n>>m;
+    int ans = (modn(3,n,m)-1%m)%m;
+    if(ans<0)
+    {
+        ans = m+ans;
+    }
+    cout<<ans<<nl;
+}
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
