58a01c6cd1d74b281816373c7579ec8443253d77
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:55:38 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1744E1 | Divisible Numbers (easy version)/176619092.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+ 
+ 
+//functions:
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,c,d;
+        cin>>a>>b>>c>>d;
+        bool acc = 0;
+        for(ll x = a+1;x<=c;x++)
+        {
+            ll y = (a*b)/gcd(x,a*b);
+            ll o = y;
+            while(y<=b)
+            {
+                y+=o;
+            }
+            if(!(y<=d&&y>b))
+            {
+                continue;
+            }
+            cout<<x<<" "<<y<<nl;
+            acc = 1;
+            break;
+        }
+        if(!acc)
+        {
+            cout<<-1<<" "<<-1<<nl;
+        }
+    }
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
