41684580da0e577cfce50f593e2685cc26fd3e57
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 11:10:50 +0100

    Committing files

---
+++ CodeForces/GNU C++17/702B | Powers of Two/167901038.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+ 
+const ll MOD = 1e9 + 7;
+ 
+//variables:
+ 
+ 
+//functions:
+ 
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    boost;
+    int n;
+    cin>>n;
+    vl pow2;
+    vl v(n);
+    map<ll,int> found;
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+        if(!found.count(v[i]))
+            found[v[i]]=0;
+        found[v[i]]++;
+    }
+    for(int i=1;i<=32;i++)
+    {
+        pow2.pb(pow(2,i));
+    }
+    ll ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        for(int co=31;co>=0;co--)
+        {
+            if(found.count(pow2[co]-v[i])&&found[pow2[co]-v[i]])
+            {
+                ans+=found[pow2[co]-v[i]];
+                if(v[i]==pow2[co]-v[i])
+                {
+                    ans--;
+                }
+            }
+        }
+    }
+    cout<<ans/2<<nl;
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
