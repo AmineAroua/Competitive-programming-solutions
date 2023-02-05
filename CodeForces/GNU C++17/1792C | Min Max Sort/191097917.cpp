b9b4cb4d17e63aae8b4a1c9c7182e8debd836789
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:38:19 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1792C | Min Max Sort/191097917.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define fore(i, y) for(ll i = 0; i < y; i++)
+#define forr(i,x,y) for(int i = x;i<=y;i++)
+#define forn(i,y,x) for(ll i = y; i >= x; i--)
+#define all(v) v.begin(),v.end()
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+#define ub upper_bound
+#define lb lower_bound
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vi v(n);
+        vi pos(n+1 , 0);
+        fore(i,n)
+        {
+            cin>>v[i];
+            pos[v[i]] = i+1;
+        }
+        if(n==1){cout<<0<<nl;continue;}
+        int ans = (n+1)/2 , l = (n+1)/2 , r = (n+2)/2;
+        // check if [l,r] is sorted
+        while(l>=1&&(l==r||(pos[l]<pos[l+1]&&pos[r-1]<pos[r])))
+        {
+            ans--;
+            l--;
+            r++;
+        }
+        cout<<ans<<nl;
+    }
+}
+ 
