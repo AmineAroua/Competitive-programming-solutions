946aaef0f9d46072a376b27f4a16db29be9041f7
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:43:09 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1770B | Koxia and Permutation/187372331.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) (v.begin(),v.end())
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int cnt = n;
+        for(int i = 0;i<n/2;i++)
+        {
+            cout<<cnt<<" "<<n+1-cnt<<" ";
+            cnt--;
+        }
+        if(n%2==1)
+        {
+            cout<<n/2+1<<" ";
+        }
+        cout<<nl;
+    }
+}
+ 
+ 
