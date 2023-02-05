46350f9c8ae8d6a0150f86de2ee1696dab2fd3ad
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:42:54 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1733A | Consecutive Sum/187513168.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define fore(i, y) for(ll i = 0; i < y; i++)
+#define forn(i, x) for(ll i = x-1; i >= 0; i--)
+#define all(v) v.begin(),v.end()
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+#define ub upper_bound
+#define lb lower_bound
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vl v(n);
+        fore(i,n)
+        {
+            cin>>v[i];
+        }
+        ll ans = 0;
+        for(int j = 0;j<k;j++)
+        {
+            ll cur = 0;
+            for(int i = j;i<n;i+=k)
+            {
+                cur = max(cur , v[i]);
+            }
+            ans+=cur;
+        }
+        cout<<ans<<nl;
+    }
+}
+ 
