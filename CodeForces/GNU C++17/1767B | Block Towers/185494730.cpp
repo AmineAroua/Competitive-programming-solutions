bf8e8964d5df23dd402a2d0c9362cef96ed0f870
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:44:47 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1767B | Block Towers/185494730.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) (v.begin(),v.end())
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ll flor(ll x)
+{
+    if(x%2==0)return x/2;
+    return x/2+1;
+}
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        ll a;
+        cin>>a;
+        vl v(n-1);
+        for(int i = 0;i<n-1;i++)
+        {
+            cin>>v[i];
+        }
+        sort(v.begin() , v.end());
+        for(int i = 0;i<n-1;i++)
+        {
+            if(v[i]<=a)continue;
+            a = flor((a+v[i]));
+        }
+        cout<<a<<nl;
+    }
+}
+ 
+ 
