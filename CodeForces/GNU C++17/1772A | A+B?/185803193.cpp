e25ee933cb1c73cdd00509a609060c0619dfea03
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:44:29 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1772A | A+B?/185803193.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<s[0]-48+s[2]-48<<endl;
+    }
+}
