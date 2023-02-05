e78c136f2393b6154dee3114f499724e8a7808d6
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:56:03 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1746B | Rebellion/176350266.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+ 
+ 
+//functions:
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vi v(n);
+        vi zero;
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        int ans = 0;
+        int i = 0,j = n-1;
+        while(i<j)
+        {
+            if(v[j]==1)
+            {
+                j--;
+                continue;
+            }
+            if(v[j]==0&&v[i]==1)
+            {
+                ans++;
+                i++;
+                j--;
+                continue;
+            }
+            if(v[i]==1&&v[j]==1)
+            {
+                i++;
+                continue;
+            }
+            i++;
+        }
+        cout<<ans<<nl;
+    }
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
