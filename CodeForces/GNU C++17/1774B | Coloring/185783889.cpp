0e5d9ba9ab58065159ec726ddb21feaf0bd391c6
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:44:36 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1774B | Coloring/185783889.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) (v.begin(),v.end())
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+int flor(int n,int k)
+{
+    if(n%k==0)return n/k;
+    return n/k+1;
+}
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m,k;
+        cin>>n>>m>>k;
+        int mx = 0 , nb = 0;
+        ll lim = flor(n,k);
+        for(int i= 0;i<m;i++)
+        {
+            int a;
+            cin>>a;
+            mx = max(a,mx);
+            nb+=a==lim;
+        }
+        if(mx>lim)
+        {
+            cout<<"no"<<nl;
+        }
+        else if((nb<=n%k&&n%k!=0)||n%k==0)
+        {
+            cout<<"yes"<<nl;
+        }
+        else
+        {
+            cout<<"no"<<nl;   
+        }
+    }
+}
+ 
