f1aed08e3394a45e492d8493266db02090b1e7c6
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 11:12:00 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1703F | Yet Another Problem About Pairs Satisfying an Inequality/165295914.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+//variables:
+ 
+//functions:
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+int compare(const void* a, const void* b)
+{
+   const int* x = (int*) a;
+   const int* y = (int*) b;
+   return (*x - *y);
+}
+ 
+//bsearch(&cible,arr,n,sizeof(int),compare);
+int bs(int j,vii &vt){
+    int l=-1,r=vt.size()+1;
+    ll s=0;
+    while(l+1<r){
+        int mid=(l+r)/2;
+        if(vt[mid].second<j){
+            s+=mid-l;
+            l=mid;
+        }
+        else {
+            r=mid;
+        }
+    }
+    return s;
+}
+ 
+int main()
+{
+    boost;
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vi v(n);
+        vii vt;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            if(i+1>v[i])vt.pb(mp(v[i],i+1));
+        }
+        ll sum=0;
+        for(int i=0;i<vt.size();i++){
+            sum+=bs(vt[i].first,vt);
+        }
+        cout<<sum<<nl;
+    }
+}
+ 
+ 
+ 
+ 
+/*
+Test 1
+INPUT:                              |OUTPUT:
+                                    |
+                                    |
+                                    |
+                                    |
+                                    |
+                                    |
+        |
+Test 2                              |
+INPUT:                              |OUTPUT:
+                                    |
+                                    |
+                                    |
+                                    |
+                                    |
+        |
+        |
+        |
+        |
+----------------------------------------------------------------------------------------
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
