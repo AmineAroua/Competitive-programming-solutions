31bbdd7d154964df77ec29f8c61ff81d2b5181cc
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:55:26 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1732A | Bestie/177664001.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+vi v(20);
+int n;
+//functions:
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+bool gcdd()
+{
+    int g = v[0];
+    for(int i=0;i<n;i++)
+    {
+        g = gcd(g,v[i]);
+    }
+    if(g==1)
+    {
+        return true;
+    }
+    return false;
+}
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        v.assign(20,0);
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        if(gcdd())
+        {
+            cout<<0<<nl;
+            continue;
+        }
+        int x = v[n-1];
+        v[n-1] = gcd(n,v[n-1]);
+        if(gcdd())
+        {
+            cout<<1<<nl;
+            continue;
+        }
+        v[n-1] = x;
+        int y = v[n-2];
+        v[n-2] = gcd(n-1,v[n-2]);
+        if(gcdd())
+        {
+            cout<<2<<nl;
+            continue;
+        }
+        cout<<3<<nl;
+    }
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
