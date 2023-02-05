ce48c60a8d153270e6cc108fe585cddc48c26750
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:58:57 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1462E1 | Close Tuples (easy version)/170755228.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) v.begin(),v.end()
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+ 
+ 
+//functions:
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vi v(n);
+        ll ans = 0;
+        forr(i,0,n-1)
+        {
+            cin>>v[i];
+        }
+        if(n<3)
+        {
+            cout<<0<<nl;
+            continue;
+        }
+        sort(all(v));
+        forn(i,n-1,2)
+        {
+            ll lb = (i+1)-(lower_bound(all(v),v[i]-2)-v.begin())-2;
+            if(lb<=0)
+                continue;
+            ans+=(lb*(lb+1))/2;
+        }
+        cout<<ans<<nl;
+    }
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
+ 
+ 
