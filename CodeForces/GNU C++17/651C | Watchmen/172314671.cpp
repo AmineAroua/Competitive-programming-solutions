6eb2ef4c5fff16f21f0a273b1ca5ebb696edc735
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:57:54 +0100

    Committing files

---
+++ CodeForces/GNU C++17/651C | Watchmen/172314671.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+//variables:
+ 
+ 
+//functions:
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int n;
+    cin>>n;
+    map<int,ll> occX;
+    map<int,ll> occY;
+    map<pair<int,int>,ll> pairs;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        occX[a]++;
+        occY[b]++;
+        pairs[mp(a,b)]++;
+    }
+    ll ans = 0;
+    for(auto p:occX)
+    {
+        ll x = p.se;
+        ans+=(x*(x-1))/2;
+    }
+    for(auto p:occY)
+    {
+        ll y= p.se;
+        ans+=(y*(y-1))/2;
+    }
+    for(auto p:pairs)
+    {
+        ll nb = p.se;
+        ans-=(nb*(nb-1))/2;
+    }
+    cout<<ans<<nl;
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
