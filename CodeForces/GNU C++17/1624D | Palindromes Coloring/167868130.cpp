01424d8541e713063425ef6350950db84b17b6f4
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 11:10:57 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1624D | Palindromes Coloring/167868130.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+//variables:
+ 
+//functions:
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+int main()
+{
+    boost;
+    int t;cin>>t;
+    while(t--){
+        int n,k;cin>>n>>k;
+        string s;
+        cin>>s;
+        vi freq(26,0);
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+        }
+        int plds = 0;
+        for(int i=0;i<26;i++){
+            plds+=freq[i]-freq[i]%2;
+        }
+        int ans=plds/k;
+        if(ans%2==0&&s.size()-plds+plds%k>=k)cout<<ans+1<<nl;
+        else if(ans)
+            cout<<ans<<nl;
+        else cout<<1<<nl;
+    }
+}
+ 
+ 
+ 
+ 
+/*
+ 
+1/2 half of the contest:
+ Read the subject in ~2 min
+ Reformulate the topic
+ solve examples
+ Think of the solution 3 min
+ Implement the solution for 20 min
+ Pseudo-code your algorithm
+ Verify your solution
+ Code your solution
+ Test your solution
+ Debug your code
+2/2 half of the contest:
+ find the easiest subtasks and try to solve them or give up
+ if there are many subtasks, the problem is easy
+ if the first subtask is difficult with no much points, the problem is difficult
+----------------------------
+!(: remember, you are the king of competitive programming
+*/
