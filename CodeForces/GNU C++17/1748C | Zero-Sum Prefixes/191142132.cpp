0829c12c89dc2883fc21c61d0843de71ad6b6b4b
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:38:11 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1748C | Zero-Sum Prefixes/191142132.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define fore(i, y) for(ll i = 0; i < y; i++)
+#define forr(i,x,y) for(int i = x;i<=y;i++)
+#define forn(i,y,x) for(ll i = y; i >= x; i--)
+#define all(v) v.begin(),v.end()
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+#define ub upper_bound
+#define lb lower_bound
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vi v(n);
+        fore(i,n)
+        {
+            cin>>v[i];
+        }
+        ll j = -1;
+        ll cur = 0;
+        ll ans = 0;
+        vector<map<ll,ll>> zero(n);
+        fore(i,n)
+        {
+            cur+=v[i];
+            if(v[i]==0)
+            {
+                j++;
+                zero[j].clear();
+            }
+            if(j!=-1)
+            {
+                zero[j][cur]++;
+            }
+            else
+                ans+=cur==0;
+        }
+        fore(i , j+1)
+        {
+            ll best = 0;
+            for(auto p:zero[i])
+            {
+                best = max(best , p.se);
+            }
+            ans+=best;
+        }
+        cout<<ans<<nl;
+    }
+}
+ 
