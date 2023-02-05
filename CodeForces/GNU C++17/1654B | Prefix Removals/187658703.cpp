7d7f702ba6b2c55c02add017bc15e46ade38c0b1
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:42:29 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1654B | Prefix Removals/187658703.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define fore(i, y) for(ll i = 0; i < y; i++)
+#define forr(i,x,y) for(int i = x;i<=y;i++)
+#define forn(i,y,x) for(ll i = y; i >= x; i--)
+#define all(v) v.begin(),v.end()
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+#define ub upper_bound
+#define lb lower_bound
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int n = s.size();
+        string ans = "" , cur = "";
+        ans+=s[n-1];
+        cur+=s[n-1];
+        set<char> in;
+        in.insert(s[n-1]);
+        forn(i,n-2,0)
+        {
+            cur+=s[i];
+            if(!in.count(s[i]))
+            {
+                ans = cur;
+            }
+            in.insert(s[i]);
+        }
+        reverse(all(ans));
+        cout<<ans<<nl;
+    }
+}
+ 
