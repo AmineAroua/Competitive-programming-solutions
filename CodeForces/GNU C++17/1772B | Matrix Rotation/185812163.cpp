8a0f74f2328cfb72d4289104ad77b46451fce5cf
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2023-02-05 10:44:27 +0100

    Committing files

---
+++ CodeForces/GNU C++17/1772B | Matrix Rotation/185812163.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vl vector<long long>
+#define vii vector<pair<int,int>>
+#define vll vector<pair<long long,long long>>
+#define pb push_back
+#define ll long long
+#define ld long double
+#define nl '\n'
+#define boost ios::sync_with_stdio(false)
+#define mp make_pair
+#define se second
+#define fi first
+#define forr(i, x, y) for(ll i = x; i <= y; i++)
+#define forn(i, x, y) for(ll i = x; i >= y; i--)
+#define all(v) (v.begin(),v.end())
+#define sz(v) v.size()
+#define clr(v,k) memset(v,k,sizeof(v))
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 1;
+ 
+ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;} // greatest common divisor (PGCD)
+ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);} // least common multiple (PPCM)
+ 
+// HERE IS THE SOLUTION
+bool check(int a,int b,int c,int d)
+{
+    return a<b&&c<d&&a<c&&b<d;
+}
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    boost;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(check(a,b,c,d)||check(c,a,d,b)||check(d,c,b,a)||check(b,d,a,c))
+        {
+            cout<<"YES"<<nl;
+        }
+        else
+        {
+            cout<<"NO"<<nl;
+        }
+    }
+}
+ 
+ 
