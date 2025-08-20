24b5914b62bece514484579d82513a0514836f66
AmineAroua <82408238+AmineAroua@users.noreply.github.com>
2025-08-20 17:29:09 +0100

    Create usefulfunctions.cpp

---
+++ C++ for beginners/usefulfunctions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    // assign(n , x) : first n values to x , O(n)
+    // push_back(x) : adds x to right , O(1)
+    // pop_back() : removes the last element , O(1)
+    // back() : value of the last element , O(1)
+    // size() , O(1)
+    // sort(v.begin() , v.end())
+    // find(v.begin() , v.end() , x) - v.begin() -> first position of x in v
+    // erase(it) : remove element at index it            O(n)
+    // erase(it1 , it2) : remove elements in [it1 , it2)
+    // insert(it , x) : insert element x before it
+}
